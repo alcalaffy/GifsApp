7f10b76cbe93fd2542a082ba7518ef812c7b1c3d
alcalaffy <81399287+alcalaffy@users.noreply.github.com>
2024-05-23 15:09:27 -0600

    Adding the http request to gyphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,35 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  constructor(private http:HttpClient){}
   private _tagsHistory:string[]=[''];
-
+  private apiKey:string='Lzaje5s143Hq3d2FAQw0Ae9NEtdcUGFB';
+  private serviceUrl:string='https://api.giphy.com/v1/gifs';
+  public gifsList:Gif[]=[];
   get tagsHistory():string[]{
     return [...this._tagsHistory];
   }
+
   searchTag(tag:string):void{
     if(tag.length===0){return;}
     this.organizeHistory(tag);
+
+    const params=new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit','10')
+      .set('q',tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
+      .subscribe(resp=>{
+        this.gifsList=resp.data;
+        console.log('gifs: ',this.gifsList);
+      })
   }
+
   private organizeHistory(tag:string){
     //minusculas para realizar busqueda
     tag=tag.toLowerCase();
@@ -24,8 +42,10 @@ export class GifsService {
 
     //si no existe en el array se agrega en la posicion 0
     this._tagsHistory.unshift(tag);
-    //
+
+    //corta el array desde el principio hasta la posicion numero 10
+    //de esta forma limita el dom a 10 elementos
+    //se iran removiendo los ultimos elementos del array ya que se inserta en la posicion 0
     this._tagsHistory=this.tagsHistory.splice(0,10);
   }
-
 }
